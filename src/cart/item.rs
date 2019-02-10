fb149ef1e9ba862c1904ea1e1a8559fadda82611
wuriyanto <wuriyanto48@yahoo.co.id>
2019-02-10 13:47:04 +0700

    add product name lifetime to cart

--- src/cart/item.rs
+++ src/cart/item.rs
@@ -1,24 +1,27 @@
 use super::product;
 
 #[derive(Debug)]
-pub struct Item {
+pub struct Item<'a, 's: 'a> {
     pub id: u32,
+    pub product: &'a product::Product<'s>,
     pub qty: u32,
     pub sub_total: f32,
 }
 
-impl Item {
-    pub fn new(id: u32, qty: u32, product: &product::Product) -> Item {
+impl<'a, 's> Item<'a, 's> {
+    pub fn new(id: u32, qty: u32, product: &'a product::Product<'s>) -> Item<'a, 's> {
         let sub_total = qty as f32 * product.price;
         Item{
             id,
+            product,
             qty,
             sub_total,
         }
     }
 
-    pub fn update(&mut self, qty: u32, product: &product::Product) {
+    pub fn update(&mut self, qty: u32, product: &'a product::Product<'s>) {
         self.qty = qty;
+        self.product = product;
         self.sub_total = qty as f32 * product.price;
     }
 
--- src/cart/mod.rs
+++ src/cart/mod.rs
@@ -3,37 +3,37 @@ pub mod item;
 use super::product;
 
 #[derive(Debug)]
-pub struct Cart<'a, 'b: 'a> {
+pub struct Cart<'a, 'b: 'a, 's: 'b> {
     pub id: u32,
-    items: &'a mut HashMap<u32, &'b mut item::Item>,
+    items: &'a mut HashMap<u32, &'b mut item::Item<'b, 's>>,
 }
 
-impl<'a, 'b>Cart<'a, 'b> {
-    pub fn new(id: u32, items: &'a mut HashMap<u32, &'b mut item::Item>) -> Cart<'a, 'b>{
+impl<'a, 'b, 's>Cart<'a, 'b, 's> {
+    pub fn new(id: u32, items: &'a mut HashMap<u32, &'b mut item::Item<'b, 's>>) -> Cart<'a, 'b, 's>{
         Cart{
             id: id,
             items: items,
         }
     }
 
-    pub fn add_item(&mut self, item: &'b mut item::Item) {
+    pub fn add_item(&mut self, item: &'b mut item::Item<'b, 's>) {
         self.items.insert(item.id, item);
     }
 
-    pub fn update_quantity(&mut self, id: u32, qty: u32, product: &product::Product){
+    pub fn update_item(&mut self, id: u32, qty: u32, product: &'b product::Product<'s>){
         if let Some(item) = self.get_item(id) {
             item.update(qty, &product);
         }
     }
 
-    pub fn get_item(&mut self, id: u32) -> Option<&mut item::Item> {
+    pub fn get_item(&mut self, id: u32) -> Option<&mut item::Item<'b, 's>> {
         match self.items.get_mut(&id) {
             Some(item) => Some(item),
             None => None
         }
     }
 
-    pub fn get_items(&self) -> &HashMap<u32, &'b mut item::Item> {
+    pub fn get_items(&self) -> &HashMap<u32, &'b mut item::Item<'b, 's>> {
         self.items
     }
 
